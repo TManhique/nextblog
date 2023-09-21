6045f4bdd112acfa42fd59f0344891d63570a38e
TManhique <tnmanhique@gmail.com>
2023-09-21 12:49:54 +0200

    nav update 2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './navbar.module.css';
 import Image from "next/image";
-import Link frfom 'next/link'
+import Link from 'next/link';
 
  function Navbar() {
   return (
